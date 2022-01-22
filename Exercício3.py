935d1e209d90bf520be00afd797a88f5cd0f35b9
fsfischer88 <felipesfischer@gmail.com>
2022-01-22 16:54:10 -0300

    ultima aula de python

---
+++ Exercício3.py
@@ -0,0 +1,5 @@
+# 3 - Usando Funções(Defs) criar um programa  “De mercadinho”, 
+# ou seja que tenha X produtos e que o usuário consiga selecionar quantos produtos 
+# quer e exiba a soma total da compra, peça a valor pago e retorne o troco
+
+
